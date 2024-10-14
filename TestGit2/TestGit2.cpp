4cb0a4697c40baec1f29debc6b307864f94cf615
Dasha <dashakonopelko@gmail.com>
2024-10-14 19:54:22 +0300

    test

--- TestGit2/TestGit2.cpp
+++ TestGit2/TestGit2.cpp
@@ -3,4 +3,9 @@
 int main()
 {
     std::cout << "Hello World!\n";
+    std::cout << "Hello World!\n";
+    std::cout << "Hello World!\n";
+    std::cout << "Hello World!\n";
+    std::cout << "Hello World!\n";
+    std::cout << "Hello World!\n";
 }
